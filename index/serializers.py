e5728390e61e3dfb245a6ff8d96532bae376a67d
chisssel <platov.artem.15@gmail.com>
2025-11-04 11:41:48 +0300

    Updating sections, adding students

--- index/serializers.py
+++ index/serializers.py
@@ -1,11 +1,42 @@
 from rest_framework import serializers
-from .models import Sections, Roles
+from .models import Sections, Roles, Users, Students
 
 
 class SectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sections
         fields = '__all__'
+
+class UserShortSerializer(serializers.ModelSerializer):
+    role_name = serializers.CharField(source='role.role_name', read_only=True)
+    class Meta:
+        model = Users
+        fields = ['id', 'username', 'first_name', 'last_name', 'patronymic', 'role_name']
+
+class StudentDetailSerializer(serializers.ModelSerializer):
+    user_details = UserShortSerializer(source='user', read_only=True)
+    class Meta:
+        model = Students
+        fields = ['id', 'user', 'user_details', 'student_start_date', 'student_end_date']
+
+class SectionWithStudentsSerializer(serializers.ModelSerializer):
+    students = serializers.SerializerMethodField()
+    students_count = serializers.SerializerMethodField()
+    active_students_count = serializers.SerializerMethodField()
+    class Meta:
+        model = Sections
+        fields = ['id','section_name','students_count','active_students_count','students']
+
+    def get_students(self, obj):
+        students = Students.objects.filter(section = obj).select_related('user__role')
+        return StudentDetailSerializer(students, many = True).data
+
+    def get_students_count(self, obj):
+        return Students.objects.filter(section=obj).count()
+
+    def get_active_students_count(self, obj):
+        return Students.objects.filter(section=obj, student_end_date__isnull = True).count()
+
 
 class RolesSerializer(serializers.ModelSerializer):
     class Meta:
--- index/views.py
+++ index/views.py
@@ -5,53 +5,83 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from index.models import Sections, Roles
-from index.serializers import SectionSerializer, RolesSerializer
-
-
-def create_list_view(model, serializer_class):
-    @api_view(['GET', 'POST'])
-    def view(request):
-        if request.method == 'GET':
-            items = model.objects.all()
-            serializer = serializer_class(items, many=True)
-            return Response(serializer.data)
-        elif request.method == 'POST':
-            serializer = serializer_class(data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    return view
-
-def detail_view(model, serializer_class):
-    @api_view(['GET', 'PUT', 'DELETE'])
-    def view(request, pk):
-        try:
-            item = model.objects.get(pk=pk)
-        except model.DoesNotExist:
-            return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
-
-        if request.method == 'GET':
-            serializer = serializer_class(item)
-            return Response(serializer.data)
-        elif request.method == 'PUT':
-            serializer = serializer_class(item, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        elif request.method == 'DELETE':
-            item.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-
-    return view
+from index.serializers import SectionSerializer, RolesSerializer, SectionWithStudentsSerializer
 
 
 def indexView(request):
     return HttpResponse('Hello World!')
 
 
-sectionsView = create_list_view(Sections, SectionSerializer)
-sectionDetailView = detail_view(Sections, SectionSerializer)
-rolesView = create_list_view(Roles, RolesSerializer)
-rolesDetailView = detail_view(Roles, RolesSerializer)
+@api_view(['GET', 'POST'])
+def sectionsView(request):
+    if request.method == 'GET':
+        sections = Sections.objects.all()
+        serializer = SectionSerializer(sections, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = SectionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def sectionDetailView(request, pk):
+    try:
+       section = Sections.objects.get(pk=pk)
+    except Sections.DoesNotExist:
+       return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = SectionWithStudentsSerializer(section)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = SectionSerializer(section, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        section.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET', 'POST'])
+def rolesView(request):
+    if request.method == 'GET':
+        roles = Roles.objects.all()
+        serializer = RolesSerializer(roles, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = RolesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def rolesDetailView(request, pk):
+    try:
+        roles = Roles.objects.get(pk=pk)
+    except Roles.DoesNotExist:
+        return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = RolesSerializer(roles)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = RolesSerializer(roles, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        roles.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
